0348565e9282f948804802d45fd644081080252a
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2013-09-26 18:57:18 +0200

    adding labels

--- addFeatureToMap.py
+++ addFeatureToMap.py
@@ -1,7 +1,7 @@
 import json,csv,re
 
 countries=[
-"UNITED STATES OF AMERICA",
+"UNITED STATES",
 "UNITED KINGDOM",
 "NORWAY",
 "FINLAND",
@@ -49,14 +49,33 @@ countries=[
 
 l=[]
 
-with open("new-countries.json") as infile:
+with open("countries-detailed.geo.json") as infile:
     inf=json.load(infile)
     
 for i in inf['features']:
-    
+    n=i['properties']['NAME'].upper()
     i['properties']['sel']=0
     
+    del i['properties']['LON']
+    del i['properties']['LAT']
+    del i['properties']['REGION']
+    del i['properties']['SUBREGION']
+    del i['properties']['AREA']
+    del i['properties']['UN']
+    del i['properties']['POP2005']
+    del i['properties']['FIPS']
+    del i['properties']['ISO2']
+    del i['properties']['ISO3']
+    
+    if(n in countries):
+        l.append(n)
+        i['properties']['opacity']=1
+    else:
+        i['properties']['opacity']=0.4
         
+print len(l)
+print len(countries)
+print list(set(countries) - set(l))
 
 with open("new-countries2.json","w") as outfile:
     json.dump(inf,outfile)
